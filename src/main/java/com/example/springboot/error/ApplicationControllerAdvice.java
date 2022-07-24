99163467a3bc41e63f40c26f9d47470d389c4b51
student <student@localhost>
2022-07-24 11:13:03 +0300

    enteger

--- src/main/java/com/example/springboot/error/ApplicationControllerAdvice.java
+++ src/main/java/com/example/springboot/error/ApplicationControllerAdvice.java
@@ -1,6 +1,10 @@
 package com.example.springboot.error;
 
 
+import com.example.springboot.exception.ForbiddenException;
+import com.example.springboot.exception.PostNotFoundException;
+import com.example.springboot.exception.UserLoginAlreadyRegisteredException;
+import com.example.springboot.exception.UserLoginNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,9 +14,31 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
 
+
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponseDTO handle(final MethodArgumentNotValidException e) {
         return new ErrorResponseDTO(ErrorResponseDTO.ARGUMENT_NOT_VALID);
     }
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ErrorResponseDTO handle(final ForbiddenException e) {
+        return new ErrorResponseDTO(ErrorResponseDTO.EXECUTE_ACCESS_FORBIDDEN);
+    }
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponseDTO handle(final PostNotFoundException e) {
+        return new ErrorResponseDTO(ErrorResponseDTO.THE_SERVER_CANNOT_FIND_THE_DATA);
+    }
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponseDTO handle(final UserLoginNotFoundException e) {
+        return new ErrorResponseDTO(ErrorResponseDTO.THE_SERVER_CANNOT_FIND_USER_DATA);
+    }
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponseDTO handle(final UserLoginAlreadyRegisteredException e) {
+        return new ErrorResponseDTO(ErrorResponseDTO.THE_SERVER_CANNOT_FIND_USER_DATA);
+    }
+
 }
--- src/main/java/com/example/springboot/error/ErrorResponseDTO.java
+++ src/main/java/com/example/springboot/error/ErrorResponseDTO.java
@@ -14,5 +14,9 @@ public class ErrorResponseDTO {
 
     public static final String SERVER_ERROR = "server_error";
     public static final String ARGUMENT_NOT_VALID = "argument_not_valid";
+    public static final String EXECUTE_ACCESS_FORBIDDEN = "Execute access forbidden";
+    public static final String THE_SERVER_CANNOT_FIND_THE_DATA = "the server cannot find the data";
+    public static final String THE_SERVER_CANNOT_FIND_USER_DATA = "the server cannot find user data";
+
     private  String  message;
 }
